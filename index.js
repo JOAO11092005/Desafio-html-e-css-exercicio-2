e9aad302f9aef6b3978bc8b2158222cbd95f6ae3
Joao Paulo <joaopaulonevesbatista@gmail.com>
2023-11-22 15:51:10 -0300

    Atualização na descricao na imagem

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.querySelector('.container').addEventListener('mouseover', () => {
+//     const message = document.querySelector('.message');
+//     message.innerText = "Em 1496, Michelangelo mudou-se para Roma e lá tornou-se um dos artistas renascentistas mais conhecidos de todos os tempos. Em Roma, ele pôde ampliar os seus conhecimentos de arte clássica e de arquitetura, produzindo algumas de suas obras mais conhecidas no período em que esteve na cidade.";
+//     message.style.visibility = 'visible';
+//   });
+  
+//   document.querySelector('.container').addEventListener('mouseout', () => {
+//     const message = document.querySelector('.message');
+//     message.style.visibility = 'hidden';
+//   });
+  
